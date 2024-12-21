46f750605ed7bfd6ff62e87318221166aa71a98a
paulosys <paulosilvasergio486@gmail.com>
2024-12-21 17:48:14 -0300

    feat: dockerize application and database with SQLAlchemy integration

---
+++ examinis/modules/__init__.py
@@ -0,0 +1,14 @@
+import os
+from typing import List
+
+
+def get_named_modules() -> List[str]:
+    """Get named modules in the current directory."""
+    excluded = {'__pycache__', '__init__.py'}
+    modules_path = os.path.abspath(os.path.dirname(__file__))
+    return [
+        module for module in os.listdir(modules_path) if module not in excluded
+    ]
+
+
+__all__ = get_named_modules()
