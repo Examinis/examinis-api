869d7d001e4bf07c1db11b3921de6ee5b3c27064
Yago César <yagocdj@gmail.com>
2025-01-29 00:50:45 -0300

    feat: add config related to CORS; add new endpoint for subject

--- examinis/app.py
+++ examinis/app.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from examinis.modules import include_routers
 
@@ -11,6 +12,19 @@ app = FastAPI(
     title='Examinis API',
     description='Examinis API documentation',
     debug=True,
+)
+
+origins = [
+    'http://localhost:4200',
+    'https://localhost:4200',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 include_routers(app)
