838eb024a1073d3acb3e38b20776fab410b60b29
Yago César <yagocdj@gmail.com>
2025-02-19 20:46:50 -0300

    feat: add filtered count method to ExamRepository for improved pagination

--- examinis/modules/exam/service.py
+++ examinis/modules/exam/service.py
@@ -45,7 +45,7 @@ class ExamService(ServiceAbstract[Exam]):
         self, params: ExamPageParams
     ) -> PagedResponseSchema[ExamListSchema]:
         items = self.repository.get_all_paginated(params)
-        total = self.repository.count_all()
+        total = self.repository.count_filtered(params)
 
         results = [ExamListSchema.from_orm(item) for item in items]
 
