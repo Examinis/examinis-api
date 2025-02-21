a85dc09cc47eae5530121e925215e9e473b92822
paulosys <paulosilvasergio486@gmail.com>
2025-02-21 13:38:52 -0300

    feat: enhance test fixtures with mock database time and session management

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,26 @@
+import datetime
+from contextlib import contextmanager
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
 from testcontainers.postgres import PostgresContainer
 
 from examinis.app import app
 from examinis.db.config import get_session
 from examinis.models.base import Base
-from tests.factories import (DifficultyFactory, QuestionFactory,
-                             SubjectFactory, UserFactory)
-
-
-@pytest.fixture
-def client() -> TestClient:
-    return TestClient(app)
+from tests.factories import (
+    DifficultyFactory,
+    QuestionFactory,
+    SubjectFactory,
+    UserFactory,
+)
 
 
 @pytest.fixture(scope='session')
 def engine():
+    """Create a Postgres container and return an SQLAlchemy engine."""	
     with PostgresContainer('postgres:17.2', driver='psycopg') as postgres:
         _engine = create_engine(postgres.get_connection_url())
 
@@ -26,7 +29,20 @@ def engine():
 
 
 @pytest.fixture
+def session(engine):
+    """Create a new database session for a test."""
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+        session.rollback()
+
+    Base.metadata.drop_all(engine)
+
+
+@pytest.fixture
 def client(session):
+    """Create a FastAPI test client with a test database session."""	
     def get_session_override():
         return session
 
@@ -37,15 +53,25 @@ def client(session):
     app.dependency_overrides.clear()
 
 
+@contextmanager
+def _mock_db_time(*, model, time=datetime.datetime(2025, 1, 1)):
+    def fake_time_handler(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_handler)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_time_handler)
+
+
 @pytest.fixture
-def session(engine):
-    Base.metadata.create_all(engine)
-
-    with Session(engine) as session:
-        yield session
-        session.rollback()
-
-    Base.metadata.drop_all(engine)
+def mock_db_time():
+    """Mock the current time for a database model."""
+    return _mock_db_time
 
 
 @pytest.fixture
@@ -90,14 +116,3 @@ def question(session):
     session.refresh(question)
 
     return question
-
-
-@pytest.fixture
-def subject(session):
-    subject = SubjectFactory()
-
-    session.add(subject)
-    session.commit()
-    session.refresh(subject)
-
-    return subject
--- tests/test_question.py
+++ tests/test_question.py
@@ -1,81 +1,113 @@
 from http import HTTPStatus
 
+from examinis.models.option import Option
+from examinis.models.question import Question
 
-def test_create_question(client, user, subject, difficulty):
-    response = client.post(
-        '/questions/',
-        json={
-            'text': 'What is the capital of France?',
-            'subject_id': subject.id,
-            'difficulty_id': difficulty.id,
-            'options': [
-                {'description': 'Paris', 'letter': 'A', 'is_correct': True},
-                {'description': 'London', 'letter': 'B', 'is_correct': False},
-                {'description': 'Berlin', 'letter': 'C', 'is_correct': False},
-                {'description': 'Madrid', 'letter': 'D', 'is_correct': False},
-            ],
-        },
-    )
+
+def test_create_question(client, user, subject, difficulty, mock_db_time):
+    with mock_db_time(model=Question), mock_db_time(model=Option):
+        response = client.post(
+            '/questions/',
+            json={
+                'text': 'What is the capital of France?',
+                'subject_id': subject.id,
+                'difficulty_id': difficulty.id,
+                'options': [
+                    {
+                        'description': 'Paris',
+                        'letter': 'A',
+                        'is_correct': True,
+                    },
+                    {
+                        'description': 'London',
+                        'letter': 'B',
+                        'is_correct': False,
+                    },
+                    {
+                        'description': 'Berlin',
+                        'letter': 'C',
+                        'is_correct': False,
+                    },
+                    {
+                        'description': 'Madrid',
+                        'letter': 'D',
+                        'is_correct': False,
+                    },
+                ],
+            },
+        )
 
     assert response.status_code == HTTPStatus.CREATED
-    # assert response.json() == {
-    #     'id': 1,
-    #     'text': 'What is the capital of France?',
-    #     'subject': {'id': subject.id, 'name': subject.name},
-    #     'difficulty': {'id': difficulty.id, 'name': difficulty.name},
-    #     'user': {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name},
-    #     'options': [
-    #         {
-    #             'id': 1,
-    #             'description': 'Paris',
-    #             'letter': 'A',
-    #             'is_correct': True,
-    #         },
-    #         {
-    #             'id': 2,
-    #             'description': 'London',
-    #             'letter': 'B',
-    #             'is_correct': False,
-    #         },
-    #         {
-    #             'id': 3,
-    #             'description': 'Berlin',
-    #             'letter': 'C',
-    #             'is_correct': False,
-    #         },
-    #         {
-    #             'id': 4,
-    #             'description': 'Madrid',
-    #             'letter': 'D',
-    #             'is_correct': False,
-    #         },
-    #     ],
-    # }
+
+    response_data = response.json()
+    assert response_data['text'] == 'What is the capital of France?'
+    assert response_data['subject'] == {'id': subject.id, 'name': subject.name}
+    assert response_data['difficulty'] == {
+        'id': difficulty.id,
+        'name': difficulty.name,
+    }
+    assert response_data['user'] == {
+        'id': user.id,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+    }
+    assert response_data['created_at'] == '2025-01-01T00:00:00'
+
+    expected_options = [
+        {'description': 'Paris', 'letter': 'A', 'is_correct': True},
+        {'description': 'London', 'letter': 'B', 'is_correct': False},
+        {'description': 'Berlin', 'letter': 'C', 'is_correct': False},
+        {'description': 'Madrid', 'letter': 'D', 'is_correct': False},
+    ]
+
+    response_options = [
+        {
+            'description': opt['description'],
+            'letter': opt['letter'],
+            'is_correct': opt['is_correct'],
+        }
+        for opt in response_data['options']
+    ]
+
+    assert response_options == expected_options
 
 
 def test_get_by_id(client, question):
-    response = client.get('/questions/1')
+    response = client.get(f'/questions/{question.id}')
 
     assert response.status_code == HTTPStatus.OK
-    # assert response.json() == {
-    #     'id': question.id,
-    #     'text': question.text,
-    #     'user': {'id': question.user.id, 'first_name': question.user.first_name, 'last_name': question.user.last_name},
-    #     'subject': {'id': question.subject.id, 'name': question.subject.name},
-    #     'difficulty': {
-    #         'id': question.difficulty.id,
-    #         'name': question.difficulty.name,
-    #     },
-    #     'options': [
-    #         {
-    #             'id': option.id,
-    #             'description': option.description,
-    #             'letter': option.letter,
-    #             'is_correct': option.is_correct,
-    #         }
-    #         for option in question.options
-    #     ],
-    # }
+
+    response_data = response.json()
+
+    assert response_data['id'] == question.id
+    assert response_data['text'] == question.text
+    assert response_data['user'] == {
+        'id': question.user.id,
+        'first_name': question.user.first_name,
+        'last_name': question.user.last_name,
+    }
+    assert response_data['subject'] == {
+        'id': question.subject.id,
+        'name': question.subject.name,
+    }
+    assert response_data['difficulty'] == {
+        'id': question.difficulty.id,
+        'name': question.difficulty.name,
+    }
+
+    expected_options = [
+        {
+            'id': option.id,
+            'description': option.description,
+            'letter': option.letter,
+            'is_correct': option.is_correct,
+        }
+        for option in question.options
+    ]
+
+    response_options = response_data['options']
+
+    assert response_options == expected_options
 
 
 def test_get_by_invalid_id(client):
